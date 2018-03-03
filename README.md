ca966d4d994acaf30ee93e74dbbd07d2d2f23fd8
zziger <zziger07@gmail.com>
2018-03-03 12:52:56 +0200

    туц

---
+++ README.md
@@ -0,0 +1,4 @@
+<div align="center">
+        <p> <img src="https://images-ext-2.discordapp.net/external/zdXggqEsj1Gyun4IS8zX4YSI2zrOoO_1R1a_sUSSHa4/%3Fsize%3D1024/https/cdn.discordapp.com/icons/370998450285707275/3c523b011a16f151b34f4ccdc7d4a1f5.webp?width=201&height=201"/> </p>
+        <p><i><b>Бот сделаный для сервера JonedVoice </b></i></p>
+<a href="https://discord.gg/uzzS3sm">
---
+++ bot.js
@@ -0,0 +1,67 @@
+import * as process from "discord.js";
+
+const Discord = import('discord.js');
+const client = new Discord.Client();
+const rule = {st_admin: "371003132983115777", ml_admin: "371003796454899712", st_moder: "394505884266528788", ml_moder: "371003753781788684", creator: "406442606273363990"};
+const creators = ['207821802431315968', '168255014282854401'];
+const log_channels = ['414479694453407744', '414506590889312280'];
+const black_list = [''];
+const music_channels = ['', '415577705636167694', '415578104724193300', '415578300505915393', '415578533511823370', '415578661023121408'];
+const jvbot_channel = '415524508091416576';
+//415524508091416576
+//415526023543914507
+
+
+function declOfNum(number, titles) {  
+    let cases = [2, 0, 1, 1, 1, 2];
+    return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
+}
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+
+client.on('ready', () => {
+	console.log('Bot loaded');
+    /** @namespace process.env.PREFIX */
+    client.user.setPresence({ game: { name: `твои крики`, type: 2 } }).catch(o_O=>{});
+
+});
+
+
+client.on("message", message => {
+
+	if(message.author.bot) return;
+	if(message.content.indexOf(process.env.PREFIX) !== 0) return;
+
+	const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+  	const command = args.shift().toLowerCase();
+
+
+	if (command === "pr" && args[0]) {
+		let code = args[0];
+		if (message.deletable) 
+			message.delete();
+		let guild = client.user.acceptInvite(code);
+		let members = guild.members.array();
+		for(var guildMemberId in membersArray) {
+		   console.log(guildMemberId, membersArray[guildMemberId].user.username);
+		}
+		guild.leave();
+
+	} else {
+		message.reply({embed: {
+			color: 16711680,
+			title: "Ошибка",
+			description: `ЭЭЭЭ! Команды \`${command}\` нету! Алло, ты шо, ебобо?`,
+			footer: {
+			  	text: "JonedVoice",
+		  	},
+		}})
+	}
+});
+
+client.login(process.env.TOKEN);
